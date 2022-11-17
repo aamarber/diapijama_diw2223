10e72f94f03733524da228ebbbc27d30b29151f2
Aarón Martín <aamarber@gmail.com>
2022-11-17 12:59:33 +0100

    Initial code

---
+++ index.js
@@ -0,0 +1,55 @@
+(function () {
+    const onInit = function() {
+        readTextFile('./web-list.json', onReadList);
+    };
+
+    const onReadList = function(webListRaw){
+        const webListObj = JSON.parse(webListRaw);
+
+        const webList = webListObj.weblist;
+
+        buildList(webList);
+    }
+
+    const buildList = function(webList){
+        webList.forEach(buildWeb);
+    }
+
+    const buildWeb = function(web){
+        const webElement = `
+        <article class='web'>
+            
+        </article>`;
+
+        let article = document.createElement('article');
+        article.className = 'web';
+
+        article.innerHTML = 
+        `<a href='${web.url}'>
+            <img src='${web.screenshot}' alt='Captura de pantalla de la web de ${web.author}'/>
+        </a>
+        <h3>Trabajo de ${web.author}</h3>`;
+
+        const webListElement = document.getElementById('web-list');
+
+        webListElement.appendChild(article);
+    }
+
+    const readTextFile = function (file, callback) {
+        var rawFile = new XMLHttpRequest();
+        rawFile.open("GET", file, false);
+        rawFile.onreadystatechange = function () {
+            if (rawFile.readyState === 4) {
+                if (rawFile.status === 200 || rawFile.status == 0) {
+                    var allText = rawFile.responseText;
+                    callback(allText);
+                }
+            }
+        }
+        rawFile.send(null);
+    };
+
+    document.addEventListener("DOMContentLoaded", function () {
+        onInit();
+    });
+})();
